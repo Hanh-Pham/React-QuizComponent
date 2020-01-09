5a5443726fd99e5b65df2a6184ce126821075908
Hanh <hph@nexiosit.com>
2020-01-09 13:48:17 +0100

    module 4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
 
 let quizData = require('./quiz_data.json');
 
@@ -13,10 +14,15 @@ class Quiz extends Component {
     }
     
     render() {
-        
+        const isQuizEnd = ((this.state.quiz_position-1)===quizData.quiz_questions.length)
         return (
           <div>
-            <QuizQuestion quiz_question={quizData.quiz_questions.find(x=>x.id===this.state.quiz_position)}/>
+                {isQuizEnd ? <QuizEnd /> :
+                    <QuizQuestion
+                        quiz_question={quizData.quiz_questions.find(
+                            x => x.id === this.state.quiz_position
+                        )}
+                    />}
           </div>
         );
     }
